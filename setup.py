0a2dd0d7453b42390b8c4337c2950efd6bf6913b
adam139 <yuejun.tang@gmail.com>
2019-11-12 20:25:50 +0800

    modify readme file name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='cms.theme',
       version=version,
       description="Chinese medical science AI system theme.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
@@ -27,12 +27,13 @@ setup(name='cms.theme',
           'setuptools',
           # -*- Extra requirements: -*-
       ],
+      extras_require={
+    'test': ['plone.app.testing',]
+        },       
       entry_points="""
       # -*- Entry points: -*-
 
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
